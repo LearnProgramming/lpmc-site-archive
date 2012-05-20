2c8b939c4b88dde301c8082dc6979a0684387db7
Joe Fredette <jfredett@gmail.com>
2012-05-19 22:40:46 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lpmc::Application.config.secret_token = 'ee40825f155bfcf9059cbcb3dfb2581bcb5c52ad6b92fd285178060f43a3f2a823382c9f5e69aa0868539fa7e313173b3dbfd91961edd883e704dbdfce375cee'
