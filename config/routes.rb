1cd0fddedcad83de49ddec119b745ae97ef6e81d
Omar Hughes <Naomarik@Omars-MacBook-Air.local>
2012-07-25 00:39:46 +0400

    added blog link to header

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 LpmcSite::Application.routes.draw do
-  resources :posts
-
   devise_for :users
 
 	root to: 'static_pages#home'
-  match '/home', to: 'static_pages#home'
-  match '/about', to: 'static_pages#about'
+  match 'home', to: 'static_pages#home'
+  match 'about', to: 'static_pages#about'
+  resources :posts
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,8 +37,6 @@ Spork.prefork do
     # automatically. This will be the default behavior in future versions of
     # rspec-rails.
     config.infer_base_class_for_anonymous_controllers = false
-
-    config.include Devise::TestHelpers, :type => :controller
   end
 
   Spork.each_run do
